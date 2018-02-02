1843bf34a75e07d2cc7157c29b70e828fc45d8b3
Marius <avramescu.marius@gmail.com>
2018-02-02 17:08:32 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitTestCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Git";
+        }
+    }
+}
